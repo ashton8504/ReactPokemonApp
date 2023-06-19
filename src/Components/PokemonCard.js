5861ee9b54cb8cd0718dc81eb9af5df86bfae393
ashton8504 <ashtonlittle8504@gmail.com>
2023-06-19 12:45:40 -0400

    update

--- src/Components/PokemonCard.js
+++ src/Components/PokemonCard.js
@@ -14,7 +14,8 @@ function PokemonCard({ name }) {
       .get(`https://pokeapi.co/api/v2/pokemon/${name}`)
       .then((response) => {
         const { height, weight, abilities, sprites } = response.data;
-        setPokemonDetails({ height, weight, abilities, image: sprites.front_default });
+        const abilityNames = abilities.map((ability) => ability.ability.name);
+        setPokemonDetails({ height, weight, abilities: abilityNames, image: sprites.front_default });
       })
       .catch((error) => {
         console.log(error);
@@ -29,16 +30,20 @@ function PokemonCard({ name }) {
     return null;
   }
 
+  const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
+
   return (
     <div className="pokemon-card">
+      <h3 className="pokemon-card-title">{capitalizedName}</h3>
+      <img src={pokemonDetails.image} alt={capitalizedName} className="pokemon-image" />
+      <div className="pokemon-card-content">
+        <p>Height: {pokemonDetails.height}</p>
+        <p>Weight: {pokemonDetails.weight}</p>
+        <p>Abilities: {pokemonDetails.abilities.join(", ")}</p>
+      </div>
       <button className="close-button" onClick={handleClose}>
         Close
       </button>
-      <h3>{name}</h3>
-      <img src={pokemonDetails.image} alt={name} className="pokemon-image" />
-      <p>Height: {pokemonDetails.height}</p>
-      <p>Weight: {pokemonDetails.weight}</p>
-      <p>Abilities: {pokemonDetails.abilities.join(", ")}</p>
     </div>
   );
 }
