84d44e18c27a4352b88645e80ca9f62b5dd0ba11
ashton8504 <ashtonlittle8504@gmail.com>
2023-06-19 10:37:03 -0400

    uploading base files

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function SearchBar({ initialValue, onSearch }) {
+  const [query, setQuery] = useState(initialValue);
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      onSearch(query);
+    }
+  };
+
+  return (
+      <div className="search-bar-container">
+      <h1>Pokemon</h1>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search Pokemon"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={handleKeyPress}
+      />
+      <button className="search-button" onClick={() => onSearch(query)}>
+        Search
+      </button>
+    </div>
+  );
+}
+
+export default SearchBar;
