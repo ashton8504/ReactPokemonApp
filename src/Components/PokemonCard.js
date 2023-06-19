74ad792819ffce860ecd4c9252b601fb5a6ce07d
ashton8504 <ashtonlittle8504@gmail.com>
2023-06-19 11:55:02 -0400

    update

--- src/Components/PokemonCard.js
+++ src/Components/PokemonCard.js
@@ -13,24 +13,32 @@ function PokemonCard({ name }) {
     axios
       .get(`https://pokeapi.co/api/v2/pokemon/${name}`)
       .then((response) => {
-        const { height, weight, abilities } = response.data;
-        setPokemonDetails({ height, weight, abilities });
+        const { height, weight, abilities, sprites } = response.data;
+        setPokemonDetails({ height, weight, abilities, image: sprites.front_default });
       })
       .catch((error) => {
         console.log(error);
       });
   }, [name]);
 
+  const handleClose = () => {
+    setPokemonDetails(null);
+  };
+
+  if (!pokemonDetails) {
+    return null;
+  }
+
   return (
     <div className="pokemon-card">
-      {pokemonDetails && (
-        <>
-          <h3>{name}</h3>
-          <p>Height: {pokemonDetails.height}</p>
-          <p>Weight: {pokemonDetails.weight}</p>
-          <p>Abilities: {pokemonDetails.abilities.join(", ")}</p>
-        </>
-      )}
+      <button className="close-button" onClick={handleClose}>
+        Close
+      </button>
+      <h3>{name}</h3>
+      <img src={pokemonDetails.image} alt={name} className="pokemon-image" />
+      <p>Height: {pokemonDetails.height}</p>
+      <p>Weight: {pokemonDetails.weight}</p>
+      <p>Abilities: {pokemonDetails.abilities.join(", ")}</p>
     </div>
   );
 }
